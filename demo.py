242b6453211b111f36a929123c6ae943e6355237
Pratik Chakravorty <pratik.chakravorty1989@gmail.com>
2024-07-14 21:26:06 +0530

    utils added

---
+++ demo.py
@@ -0,0 +1,12 @@
+from us_visa.logger import logging
+from us_visa.exception import USvisaException
+
+logging.info("welcome to our custom log")
+
+try:
+    a = 2/0
+except Exception as e:
+    raise USvisaException(e,sys)
+
+
+
